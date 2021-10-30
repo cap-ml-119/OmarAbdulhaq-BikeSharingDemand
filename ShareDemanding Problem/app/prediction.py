dfad3f1e3a29893b0a8a5fa532279333fc27585b
OmarAbdulhaq <68372273+OmarAbdulhaq@users.noreply.github.com>
2021-10-30 23:57:42 +0300

    Delete prediction.py

--- ShareDemanding Problem/app/prediction.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pickle
-
-class PickledModel():
-
-    model = pickle.load(open('model.sav', 'rb'))
-
-    def SimplePred(self, data):
-        return self.__model.predict([data])
